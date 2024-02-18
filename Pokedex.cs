6dda11433ee1233c0ae08b761bc49c0c48c99f0e
Evan <evanmcintyre0427@gmail.com>
2024-02-18 17:28:00 -0500

    Add files via upload

--- Pokedex.cs
+++ Pokedex.cs
@@ -1,5 +1,22 @@
 ﻿class Pokedex
 {
-    List<Pokemon> myPokedex =
+    //MyPokedex List
+    List<Pokemon> myPokedex = new List<Pokemon>();
+
+    //Add To Dex Method
+    public void addToDex(Pokemon p)
+    {
+        Pokemon addp = new Pokemon();
+    }
+
+    //Override To String Method that returns ONE POKEMON PER LINE
+    public override string ToString()
+    {
+        foreach (Pokemon p in myPokedex)
+        {
+            Console.WriteLine(p.ToString());
+        }
+        return myPokedex.ToString();
+    }
 }
 
